5a5d04a9204e49f3ed600bd11123f4eeb03c3dc9
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-11-19 11:49:13 +0700

    injectmodel user to service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
